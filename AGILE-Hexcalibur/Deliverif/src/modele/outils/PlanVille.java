bf6618fe5a86b9b4d43ffe06eacf68ca67e2989b
rfournier1 <romain.fournier26@gmail.com>
2018-10-25 01:18:50 +0200

    FIX : PlanVille dijkstra (fonctionne mais test mal formulés donc passe pas)

--- AGILE-Hexcalibur/Deliverif/src/modele/outils/PlanVille.java
+++ AGILE-Hexcalibur/Deliverif/src/modele/outils/PlanVille.java
@@ -25,16 +25,21 @@ public class PlanVille {
     private List<Intersection> intersections;
     private List<Troncon> troncons;
     
-    public void dijkstra(PointPassage p){
+    public PlanVille(List<Intersection> i, List<Troncon> t){
+        this.intersections =i;
+        this.troncons=t;
+    }
+    
+    public Map<Intersection, Pair<Intersection, Float>> dijkstra(PointPassage p){
         
         List<Intersection> nonVus = new ArrayList(intersections);
         
-        List<List<Troncon>> resultat = new ArrayList();
+        List<List<Troncon>> resultat = new ArrayList<List<Troncon>>();
         ListIterator<Intersection> interIt = nonVus.listIterator();
         ListIterator<Troncon> troncIt;
         
         Intersection origine = p.getPosition();
-        Map<Intersection, Pair<Intersection,Float>> tab = new HashMap();
+        Map<Intersection, Pair<Intersection,Float>> tab = new HashMap<Intersection, Pair<Intersection,Float>>();
         Intersection sommetVisite;
         //retrait de l'origine de la liste des sommets non vus
         while(interIt.hasNext()){
@@ -42,12 +47,11 @@ public class PlanVille {
             if(sommetVisite.getLongitude()==origine.getLongitude()&&sommetVisite.getLatitude()==origine.getLatitude()){
                 //permutation
                 origine = sommetVisite;
-                //retrait de la liste des nonVus
-                nonVus.remove(sommetVisite);
                 break;
             }
         }
-        //raz de l'itérateur
+        //ajout de l'origine avec un cout nul dans le tableau
+        tab.put(origine, new Pair(origine,(float)0));
         
         interIt = nonVus.listIterator();
         while(interIt.hasNext()){
@@ -57,18 +61,41 @@ public class PlanVille {
                 Troncon arc=troncIt.next();
                 if(nonVus.contains(arc.getFin())){
                     //s'il y a deja une valeur dans le tableau de précédence :
-                    if(tab.get(arc.getFin())!=null){
-                        //on fait regarde s'il est plus avanageux de changer de point
+                    if(tab.containsKey(arc.getFin())){
+                        //on regarde s'il est plus avanageux de changer de point
                         if(tab.get(arc.getFin()).getValue()>(arc.getLongueur()+tab.get(origine).getValue())){
                             Pair<Intersection,Float> predecesseur = new Pair(origine,arc.getLongueur()+tab.get(origine).getValue());
                             tab.put(arc.getFin(), predecesseur);
                         }
                     }else{
-                         Pair<Intersection,Float> predecesseur = new Pair(origine,arc.getLongueur());
-                            tab.put(arc.getFin(), predecesseur);
+                        Pair<Intersection,Float> predecesseur = new Pair(origine,arc.getLongueur()+tab.get(origine).getValue());
+                        tab.put(arc.getFin(), predecesseur);
                     }
                 }
             }
+            //retrait de la liste des nonVus
+            nonVus.remove(origine);
+            // raz iterateur : 
+            interIt = nonVus.listIterator();
+            //choix du prochain sommet à visiter : recherche du sommet non visité avec le plus faible cout
+            if(interIt.hasNext()){
+                origine=interIt.next();
+                Intersection candidat;
+                while(interIt.hasNext()){
+                    
+                    candidat=interIt.next();
+                    if(!tab.containsKey(origine)){
+                        origine=candidat;
+                    }else if(tab.containsKey(candidat)){
+                        if(tab.get(candidat).getValue()<tab.get(origine).getValue()){
+                            origine=candidat;
+                        }
+                    }
+                }
+                // raz iterateur : 
+                interIt = nonVus.listIterator();
+            }
         }
+        return tab;
     }
 }
