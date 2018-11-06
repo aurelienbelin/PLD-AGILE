9497409b5a6508304ac7ec0cb17b55f9a55a995e
Elise <Elise@10.43.5.246>
2018-11-06 20:46:39 +0100

    Merge origin/master
    Conflicts:
    	AGILE-Hexcalibur/Deliverif/src/deliverif/Deliverif.java
    	AGILE-Hexcalibur/Deliverif/src/deliverif/EcouteurBoutons.java
    	AGILE-Hexcalibur/Deliverif/test/controleur/ControleurTest.java
    	AGILE-Hexcalibur/Deliverif/test/modele/flux/LecteurXMLTest.java
    	AGILE-Hexcalibur/Deliverif/test/modele/outils/GestionLivraisonTest.java

--- AGILE-Hexcalibur/Deliverif/src/deliverif/Deliverif.java
+++ AGILE-Hexcalibur/Deliverif/src/deliverif/Deliverif.java
@@ -136,7 +136,7 @@ public class Deliverif extends Application {
         
         creerPanelDroit();   
         //Il faudra ajouter la vue graphique
-        vueGraphique = new VueGraphique(this.gestionLivraison);
+        vueGraphique = new VueGraphique(this.gestionLivraison, this.ecouteurBoutons);
         vueGraphique.setLayoutX(0);
         vueGraphique.setLayoutY(115);
         
